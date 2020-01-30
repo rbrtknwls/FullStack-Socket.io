42aaccfefd17154eaee5db87c2c0daf1541b0833
Robert Knowles <robbie.knowles@ucc.on.ca>
2020-01-30 13:11:36 -0500

    shittttt

--- nodeserver/fserver/front/app.js
+++ nodeserver/fserver/front/app.js
@@ -6,16 +6,14 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+io.on('connect', onConnect);
+
+function onConnect(socket){
+    socket.on('a', function(msg){
+        console.log("ahh");
+    });
+}

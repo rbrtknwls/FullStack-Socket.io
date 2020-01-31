1612c477bdb035b18dc36cf09b66ae3ec7293d94
Robert Knowles <robbie.knowles@ucc.on.ca>
2020-01-31 08:33:16 -0500

    works? works!

--- nodeserver/fserver/front/app.js
+++ nodeserver/fserver/front/app.js
@@ -2,18 +2,42 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var usernames = {};
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.on('connect', onConnect);
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
 
-function onConnect(socket){
-    socket.on('a', function(msg){
-        console.log("ahh");
-    });
+function disname(dic){
+    console.log("--- nit ---")
+    for (var key in dic){
+        console.log(key +":" +dic[key])
+    }
 }
+    
+
+io.on('connection', function(socket){
+
+    console.log('Yay, connection was recorded')
+    
+    socket.on('userset', function (uname, id) {
+        usernames[id] = uname
+        disname(usernames);
+        
+    });
+    
+    socket.on('msend', function (message, id) {
+        if (id in usernames) io.emit("message", usernames[id] + ":" + message);
+        else io.emit("message", "No_User_Set:" + message)
+    });
+    //handling disconnects
+    socket.on('disconnect', function() {
+       io.emit('chat message', 'some user disconnected');
+    });
+
+});
+

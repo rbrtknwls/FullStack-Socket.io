aa0b6f659b517a25eb8d8bd1c4adc7db8a4131ac
Robert Knowles <robbie.knowles@ucc.on.ca>
2020-02-07 22:51:17 -0500

    18!

--- uoft/app.js
+++ uoft/app.js
@@ -1,26 +1,80 @@
+/// IMPORTS AND DUMB STUFF
 var express = require('express');
-var socketIO = require('socket.io');
+var AWS = require('aws-sdk')
 var path = require('path');
+var fs = require('fs');
+var atob = require('atob')
 
-const fs = require('fs');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
+// CONSTANTS AND API KEYS
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const config = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "us-east-2"
+};
+const rek = new AWS.Rekognition(config)
 
-server = express();
 
-server.get('/', function(req, res) {
+
+// PAGE BUILDING STUFF
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
-// other pages
 
 var publicPath = path.join(__dirname, 'public');
 
-server.get('/sign-in', function (req, res) {
+app.get('/sign-in', function (req, res) {
   res.sendFile(path.join(publicPath + '/done.html'));
 });
 
 server.listen(PORT);
+console.log("CHECKING PORT " + PORT +"!")
+
+//SOCKET IO STUFF
+io.on('connection', function(socket){
+    socket.on("fbbox", function (data) {
+        var imgbyte = getBinary(data);
+        var rekognitionRequest = {
+          CollectionId: "thing",
+          FaceMatchThreshold: 85, 
+          Image: {
+            Bytes: imgbyte
+          },
+          MaxFaces: 1
+        };
+        
+        rek.searchFacesByImage(rekognitionRequest, function(err,data){
+            if (err) console.log(err);
+            else{
+                var bbox = data["SearchedFaceBoundingBox"];
+                console.log(bbox);
+                io.emit("returnbbox", bbox)
+            }
+        });
+        
+    });
+    console.log("USER CONNECTED")
+});
+
+
+function getBinary(base64Image) {
+  var binaryImg = atob(base64Image);
+  var length = binaryImg.length;
+  var ab = new ArrayBuffer(length);
+  var ua = new Uint8Array(ab);
+  for (var i = 0; i < length; i++) {
+    ua[i] = binaryImg.charCodeAt(i);
+  }
+
+  return ab;
+}
+
+
+
 
 
 

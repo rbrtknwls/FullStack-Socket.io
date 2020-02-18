406ab5d9c29684eda3ef0767ba5043f16f3a6f3c
Robert Knowles <robbie.knowles@ucc.on.ca>
2020-02-18 12:14:08 -0500

    danke

--- uoft/app.js
+++ uoft/app.js
@@ -1,62 +1,66 @@
-/// IMPORTS AND DUMB STUFF
+/// IMPORTS AND PRE REQS
 var express = require('express');
 var AWS = require('aws-sdk')
 var path = require('path');
 var fs = require('fs');
 var atob = require('atob')
-
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var sslRedirect = require('heroku-ssl-redirect');
 
 // CONSTANTS AND API KEYS
 const PORT = process.env.PORT || 3000;
 const config = {
+    accessKeyId: "AKIA3C5KRETDNQ57P6VW",
+    secretAccessKey: "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",
     region: "us-east-2"
 };
+
+// Instancate OBJECTS
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 const rek = new AWS.Rekognition(config)
 
 
+
+
+/* FORCE HTTPS 
+|Reason|: video streaming lib only works over https
+|Ref|: https://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect */
+app.use(sslRedirect());
 
 // PAGE BUILDING STUFF
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-var publicPath = path.join(__dirname, 'public');
-
 app.get('/sign-in', function (req, res) {
-  res.sendFile(path.join(publicPath + '/done.html'));
+  res.sendFile(path.join(__dirname + '/public/done.html'));
 });
 
-server.listen(PORT);
-console.log("CHECKING PORT " + PORT +"!")
 
-//SOCKET IO STUFF
+
+server.listen(PORT);
+console.log("CHECKING PORT " + PORT)
+
+
 io.on('connection', function(socket){
-    socket.on("fbbox", function (data) {
-        var imgbyte = getBinary(data);
-        var rekognitionRequest = {
-          CollectionId: "thing",
-          FaceMatchThreshold: 85, 
-          Image: {
-            Bytes: imgbyte
-          },
-          MaxFaces: 1
-        };
-        
-        rek.searchFacesByImage(rekognitionRequest, function(err,data){
-            if (err) console.log(err);
-            else{
-                var bbox = data["SearchedFaceBoundingBox"];
-                io.emit("returnbbox", bbox)
-            }
-        });
+    
+    /* Recieve Image + Meta Data
+    |Reason|: We need stringifyed data so that we can authenticate our users
+    |Outcomes|: We will pass on the three outcomes found in our check functions sections*/
+    socket.on("pass_server_image", function (img, date) {
+        console.log("Begin func: pass_server_image");
+        console.log(date);
         
     });
     console.log("USER CONNECTED")
 });
 
+// FUNCTIONS //
+
+/* -- Image Processing Functions --
+|Reason|: These functions should be for the first part of the authentication process, we want to stringfy the photo that we got from the socket and do our processes on that
+|Outcomes|: NULL (No processing done with these functions)*/
 
 function getBinary(base64Image) {
   var binaryImg = atob(base64Image);
@@ -69,6 +73,18 @@ function getBinary(base64Image) {
 
   return ab;
 }
+
+/* -- Check Functions --
+|Reason|: We want to compare the image that we currently have to a collection (which represents all authenticated users), to see if theres a match
+|Outcomes|: 1) Image match in System -> Login
+            2) Image match not in System -> Create Account
+            3) Invalid Image -> Back to /sign_in*/
+
+// 3
+function datecomp (imgdate){
+    console.log(imgdate);
+}
+
 
 
 
